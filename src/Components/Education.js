a24bad57ff76356abdcb031cec0b4c9131184199
Jatin Rastogi <jatinrastogi08@gmail.com>
2021-06-22 19:23:16 +0530

    added aboutpage

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,4 +1,7 @@
 import * as ioicons from 'react-icons/io'
+import {Link} from 'react-router-dom'
+import * as bsIcons from 'react-icons/bs';
+
 const Education = () => {
     return (
         <div className="education">
@@ -6,17 +9,52 @@ const Education = () => {
                 EDUCATION
             </h1>
             <div className="education-1">
-                <ioicons.IoIosSchool size={50} color="black"/> 
+                <div className="education-button1">
+                <Link to="/about">
+                    <button>
+                        <bsIcons.BsArrowLeft size={50} color="black"/>
+                    </button>
+                </Link>
+                </div>
+                
                 <section>
+                   <ioicons.IoIosSchool size={50} color="black" float="left"/> 
                     <h3>
                     THE INDIAN SCHOOL
                     </h3>
                     <p>
                         (2006-2018)
                     </p>
+                    <p>
+                        New Delhi,India
+                    </p>
                     <hr/>
                 </section>
+                <div className="education-button2">
+                <Link to="/skill">
+                    <button>
+                        <bsIcons.BsArrowRight size={50} color="black"/>
+                    </button>
+                </Link>
+                </div>
+                
+                
     
+            </div>
+            <div className="education-2">
+                <img src="srm.png" />
+                <section>
+                    <h3>
+                    SRM Institute of Science and Technology
+                    </h3>
+                    <p>
+                        (2019-2023)
+                    </p>
+                    <p>
+                        Kattankulathur,Chennai,India
+                    </p>
+                    <hr/>
+                </section>
             </div>
         </div>
     )
