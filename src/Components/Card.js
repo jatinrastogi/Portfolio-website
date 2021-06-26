535bae7733c2a24c3272a088e654ad1851e0b464
Jatin Rastogi <jatinrastogi08@gmail.com>
2021-06-26 14:40:54 +0530

    added horizontal scroll

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({card_number}) => {
     return (
         <div style={styles.card}>
-            card
+            {card_number}
         </div>
     )
 }
 
 const styles = {
     card : {
-        width: "350px",
-        height: "200px",
-        backgroundColor: "red",
+        width: "500px",
+        height: "300px",
+        backgroundColor: "blue",
         border: "2px solid black",
         boxSizing: "border-box",
         fontSize: "2.5em",
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {motion} from 'framer-motion'
 import Card from "./Card"
-const line = "EXPERIENCE"
+import * as biIcons from "react-icons/bi"
+const line = "PROJECTS"
 const sentence = {
     hidden: {opacity:1},
     visible : {
@@ -23,12 +24,38 @@ const letter = {
 class Project extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             
+             current_card : 0
         }
     }
-    
+
+
+    componentDidMount() {
+        let first_card_clone = this.card_container.children[0].cloneNode(true)
+        let last_Card_clone = this.card_container.children[this.card_container.children.length-1].cloneNode(true)
+
+        this.card_container.insertBefore(last_Card_clone,this.card_container.children[0])
+        this.card_container.append(first_card_clone);
+        this.card_container.style.transitionDuration = "0.0s";
+        this.card_container.style.transform = `translate(-${500}px)`
+    }
+
+    handle_next = () => {
+
+        if(this.state.current_card < this.card_container.children.length - 1){
+        let new_current_card = this.state.current_card + 1;
+
+        this.setState({current_card:new_current_card}, () => {
+            this.card_container.style.transitionDuration = "0.5s";
+            this.card_container.style.transform = `translate(-${500*this.state.current_card}px)`
+        });
+    }
+        else{
+            return;
+        }
+    }
+
     render() {
         return (
             <div className="education" >
@@ -43,24 +70,49 @@ class Project extends Component {
                     })
                 }
                 </motion.header>
+            <div className="project">
+
             <div className="view-port" style={styles.view_port}>
-                <div className="card-container" style={styles.card_container}>
-                    <Card/>
+            <div className="project-button-1">
+                <button onClick={this.handle_previous}>
+                    <biIcons.BiLeftArrow size={30} color="black"/>
+                </button>
+            </div>
+                <div ref={ref_id => this.card_container=ref_id} className="card-container" style={styles.card_container}>
+                    <Card card_number="0"/>
+                    <Card card_number="1"/>
+                    <Card card_number="2"/>
+                    <Card card_number="3"/>
+
                 </div>
+
+            </div>
+            <div className="project-button-2">
+                <button onClick={this.handle_next}>
+                    <biIcons.BiRightArrow size={30} color="black"/>
+                </button>
+            </div>
             </div>
             </div>
         )
     }
 }
+
+
 const styles = {
     view_port : {
-        transform: 'translate(-50%, -50%)',
-        width: '350px',
-        height: '200px',
-        backgroundColor: 'red',
-        position: "absolute",
-        left: "50%",
-        top: "50%"
+        //transform: 'translate(-50%, -50%)',
+        display:'flex',
+        // justifyContent: 'center',
+        width: '500px',
+        height: '300px',
+        // backgroundColor: 'red',
+        marginTop: '50px',
+        // position: "absolute",
+        // left: "50%",
+        // top: "50%"
+        overflow:"hidden",
+        flexDirection: 'row'
     },
     card_container: {
         display: 'flex',
